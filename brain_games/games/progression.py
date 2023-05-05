df269da2e69bd14aa9b92cd9271c80e57d502f46
administrator <andrey.samarin88@gmail.com>
2023-05-05 10:22:50 +0300

    some change

---
+++ brain_games/games/progression.py
@@ -0,0 +1,30 @@
+import random
+
+
+def f_question():
+    print('What number is missing in the progression?')
+
+
+correct_answer = ''
+
+
+def f_game():
+    global correct_answer
+    prog = []
+    k = random.randint(5, 10)  # progression size
+    d = random.randint(5, 10)  # progression difference
+    i = 0  # progression element number
+    prog.append(random.randint(5, 10))  # the first element of the progression
+    u = random.randint(0, k - 1)  # the number of the unknown element of the progression
+    while i < k - 1:
+        prog.append(prog[i] + d)
+        i += 1
+    correct_answer = str(prog[u])
+    prog[u] = '..'
+    task = f'{prog}'
+    print(f'Question: {task}')
+
+
+def f_cor_ans() -> str:
+    global correct_answer
+    return correct_answer
---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+from brain_games.engine import logic
+from brain_games.games import progression
+
+
+def main():
+    logic(progression)
+
+
+if __name__ == '__main__':
+    main()
